150292ebdc3f314c1ce8b8c352fc4fc6ebfaca2b
alexfrancow <alejandrofrancovazquez@gmail.com>
2018-07-20 09:24:03 +0000

    Android redirect captive portal

--- app/views.py
+++ app/views.py
@@ -107,7 +107,7 @@ def download_file():
 @android_blueprint.route('/generate_204')
 @android_blueprint.route('/gen_204')
 def android():
-    return(''), 200
+    return redirect("http://10.0.0.1/", code=302)
 
 if __name__ == '__main__':
     app.run(debug= True)
